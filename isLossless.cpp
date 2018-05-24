42edffe480567dc22612e11e3b673cabe34f79a7
Marshall Clow <marshall@idio.com>
2018-05-23 17:06:29 -0700

    Abstract out the 'walking' of an M4a file

--- isLossless.cpp
+++ isLossless.cpp
@@ -1,124 +1,47 @@
 #include <iostream>
 #include <cstdio>
 #include <limits.h>
-#include <set>
 
-#include "file_helpers.hpp"
+#include "m4a_file.hpp"
 
 bool gPrintLossless = true;
 bool gDebug = false;
 
-class MPAFile {
-public:
-	typedef uint32_t AtomType;
-	typedef uint32_t AtomSize;
-	typedef void (*AtomProc) (const MPAFile *f, AtomType atom, size_t start, size_t length);
+//	We're looking for a 'stsd' atom
+struct AtomCallback {
+	int isLossless;
+	AtomCallback() { isLossless = -1; }
 	
-	MPAFile (const char *fileName) : file(fileName),
-							containers_({'cmov', 'edts', 'trak', 'mdia', 'minf', 
-							             'moov', 'stbl', 'rmda', 'rmra', 'stbl',
-							             'trak'})
+	void operator() (const M4AFile *f, M4AFile::AtomType atom, size_t start, size_t length, size_t depth)
+	{
+		if ( gDebug )
+			std::cout << std::string(depth, ' ') << "'" << f->AtomName(atom) << "' (" << length << " bytes)" << std::endl;
+
+		if ( 'stsd' == atom )
 		{
-		if (GetAtomType(0) != 'ftyp')
-			throw std::runtime_error("does not have 'ftyp' as the first atom");			
+			if ( gDebug )
+				std::cout << "Found a 'stsd' at offset " << start << std::endl;
+			if ( length < 16 )
+				throw std::runtime_error("'stsd' atom is too small");
+			isLossless = f->read32 ( start + 12 ) == 0x616C6163 ? 1 : 0;
 		}
-
-	~MPAFile() {}
-	
-	AtomSize GetAtomSize(size_t offset) const { return read32(offset); }
-	AtomType GetAtomType(size_t offset) const { return read32(offset + 4); }
-	size_t size() const { return file.size(); }
-
-	const std::set<AtomType> & containers() const { return containers_; }
-	bool AtomIsContainer(AtomType at) const { return containers_.find(at) != containers_.end();}
-
-	std::string AtomName(AtomType at) const {
-		std::string retVal ( "    " );
-		const unsigned char *p = (const unsigned char *) &at;
-		for (int i = 3; i >= 0; --i)
-			retVal [ i ] = *p++;
-		return retVal;
-		}
-
-	template <typename CB>
-	void ForEachAtomDo (CB cb) const { ForEachAtomDo(0, size(), cb); }
-	
-	template <typename CB>
-	void ForEachAtomDo (size_t start, size_t length, CB cb) const {
-		size_t offset  = start;
-		size_t end     = start + length;
-	
-		while (offset < end) {
-			MPAFile::AtomType at = GetAtomType(offset);
-			MPAFile::AtomSize sz = GetAtomSize(offset);
-
-			if (end - offset < sz)
-				throw std::runtime_error(std::string("Atom '") + AtomName(at) + "' has a size too big");
-			else if (sz < 8)
-				throw std::runtime_error(std::string("Atom '") + AtomName(at) + "' has a size too small");
-			
-			cb (this, at, offset + 8, sz - 8);
-			offset += sz;
-			}
-		}
-
-	uint32_t read32(size_t offset) const {
-		uint32_t retVal = 0;
-		const unsigned char *p = file.base() + offset;
-	
-		for (int i = 0; i < 4; ++i) {
-			retVal <<= 8;
-			retVal |= *p++;
-		}
-
-	return retVal;
 	}
-
-private:
-	MMFile file;
-	std::set<AtomType> containers_;
 };
 
 
-struct AtomCallback {
-	static int cbIsLossless;
-	AtomCallback(int indent = 0) : indentStr(indent, ' ') { cbIsLossless = -1; }
-	
-	void CheckForLossless ( const MPAFile *f, long start, long length ) {
-		if ( gDebug )
-			std::cout << "Found a 'stsd' at offset " << start << std::endl;
-		cbIsLossless = f->read32 ( start + 12 ) == 0x616C6163 ? 1 : 0;
-		}
-
-
-	void operator() (const MPAFile *f, MPAFile::AtomType atom, size_t start, size_t length) {
-		if ( gDebug )
-			std::cout << indentStr << "'" << f->AtomName(atom) << "' (" << length << " bytes)" << std::endl;
-
-		if ( 'stsd' == atom )
-			CheckForLossless ( f, start, length );
-		else if (f->AtomIsContainer (atom))
-			f->ForEachAtomDo ( start, length, AtomCallback(indentStr.size() + 2) );		
-		}
-	std::string indentStr;
-	};
-
-int AtomCallback::cbIsLossless;
-
-char toHex(int i) { return "0123456789ABCDEF"[i]; }
-
 void ProcessAFile(const char *fileName) {
 	try {
-		MPAFile f(fileName);
+		M4AFile f(fileName);
 		if (gDebug) {
 			std::cout << "Processing " << fileName << std::endl;
 			std::cout << "File  is " << f.size() << " bytes long" << std::endl;
 			}
 
-		f.ForEachAtomDo(AtomCallback{});
-		if (AtomCallback::cbIsLossless < 0)
+		AtomCallback cb;
+		f.ForEachAtomDo(std::ref(cb));
+		if (cb.isLossless < 0)
 			throw std::runtime_error("does not have a 'stsd' atom");
-		else if ( AtomCallback::cbIsLossless == gPrintLossless )
+		else if ( cb.isLossless == gPrintLossless )
 			std::cout << fileName << std::endl;
 		}
 	catch (const std::runtime_error &e) {
@@ -129,7 +52,7 @@ void ProcessAFile(const char *fileName) {
 int main( int argc, char *argv [] )
 {
 	if ( argc == 1 ) {
-		std::cout << "Usage: " << argv[0] << " [-!] <list of files" << std::endl;
+		std::cout << "Usage: " << argv[0] << " [-!] [-d] <list of files" << std::endl;
 		std::cout << "  prints the path of files that encoded with Apple lossless encoder" << std::endl;
 		std::cout << "  if -! is specified, prints the path of files NOT encoded lossless" << std::endl;
 		return 0;
---
+++ m4a_file.hpp
@@ -0,0 +1,83 @@
+#include <set>
+
+#include "file_helpers.hpp"
+
+//	See https://wiki.multimedia.cx/index.php/QuickTime_container
+
+class M4AFile {
+public:
+	typedef uint32_t AtomType;
+	typedef uint32_t AtomSize;
+	
+	M4AFile (const char *fileName) : file(fileName),
+							containers_({'cmov', 'edts', 'trak', 'mdia', 'minf', 
+							             'moov', 'stbl', 'rmda', 'rmra', 'stbl',
+							             'trak'})
+		{
+		if (GetAtomType(0) != 'ftyp')
+			throw std::runtime_error("does not have 'ftyp' as the first atom");			
+		}
+
+	~M4AFile() {}
+	
+	AtomSize GetAtomSize(size_t offset) const { return read32(offset); }
+	AtomType GetAtomType(size_t offset) const { return read32(offset + 4); }
+	size_t size() const { return file.size(); }
+
+	const std::set<AtomType> & containers() const { return containers_; }
+	bool AtomIsContainer(AtomType at) const { return containers_.find(at) != containers_.end();}
+
+	std::string AtomName(AtomType at) const {
+		std::string retVal ( "    " );
+		const unsigned char *p = (const unsigned char *) &at;
+		for (int i = 3; i >= 0; --i)
+			retVal [ i ] = *p++;
+		return retVal;
+		}
+
+	template <typename CB>
+	void ForEachAtomDo (CB cb) const { ForEachAtomDo(0, size(), 0, cb); }
+	
+	template <typename CB>
+	void ForEachAtomDo (size_t start, size_t length, size_t depth, CB cb) const {
+		size_t offset  = start;
+		size_t end     = start + length;
+	
+		while (offset < end) {
+			AtomType at = GetAtomType(offset);
+			AtomSize sz = GetAtomSize(offset);
+
+			if (end - offset < sz)
+				throw std::runtime_error(std::string("Atom '") + AtomName(at) + "' has a size too big");
+			else if (sz < 8)
+				throw std::runtime_error(std::string("Atom '") + AtomName(at) + "' has a size too small");
+			
+			cb (this, at, offset + 8, sz - 8, depth);
+			if (AtomIsContainer (at))
+				ForEachAtomDo (offset + 8, sz - 8, depth + 1, cb);		
+			
+			offset += sz;
+			}
+		}
+
+	const uint8_t * getPointerTo(size_t offset) const
+	{
+		return file.base() + offset;
+	}
+
+	uint32_t read32(size_t offset) const {
+		uint32_t retVal = 0;
+		const unsigned char *p = file.base() + offset;
+	
+		for (int i = 0; i < 4; ++i) {
+			retVal <<= 8;
+			retVal |= *p++;
+		}
+
+	return retVal;
+	}
+
+private:
+	MMFile file;
+	std::set<AtomType> containers_;
+};
